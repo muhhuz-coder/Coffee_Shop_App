76b8abda0449b08aa2aacb69d376b17b354d851f
Muhammad Huzaifa <110600384+muhhuz-coder@users.noreply.github.com>
2024-12-26 17:08:21 -0800

    df

--- android/app/src/main/java/com/coffee_shop_app/MainActivity.kt
+++ android/app/src/main/java/com/coffee_shop_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.coffee_shop_app
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
+import org.devio.rn.splashscreen.SplashScreen; // Import this for splash screen functionality
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
@@ -11,6 +13,12 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this); 
+    super.onCreate(savedInstanceState)
+  }
+
   override fun getMainComponentName(): String = "Coffee_Shop_App"
 
   /**
